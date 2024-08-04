1610d6cd475a4540662901f9425d068884e169ef
Flynotfly <Alteria2004@gmail.com>
2024-08-04 16:30:41 +0400

    add 'add' and 'save' methods to Cart

--- cart/cart.py
+++ cart/cart.py
@@ -13,3 +13,22 @@ class Cart:
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, override_quantity=False):
+        '''
+        Add a product to the cart or update its quantity
+        '''
+        product_id = (product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': 0,
+                'price': str(product.price),
+            }
+        if override_quantity:
+            self.cart[product_id] = quantity
+        else:
+            self.cart[product_id] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
