ddf5f29c9e4ca7101e287906965d2950ee70a4db
Flynotfly <Alteria2004@gmail.com>
2024-08-04 08:51:50 +0400

    typo

--- shop/admin.py
+++ shop/admin.py
@@ -10,7 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'avaliable', 'created', 'updated', 'category']
-    list_filter = ['avaliable', 'created', 'updated']
-    list_editable = ['price', 'avaliable']
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'category']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
