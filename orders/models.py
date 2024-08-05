de718c3f48ae59ed5c2ad6b97085d96445c72e88
Flynotfly <Alteria2004@gmail.com>
2024-08-05 10:31:44 +0400

    Create OrderItem model

--- orders/models.py
+++ orders/models.py
@@ -23,3 +23,28 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        'shop.Product',
+        related_name='order_items',
+        on_delete=models.CASCADE
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.price * self.quantity
