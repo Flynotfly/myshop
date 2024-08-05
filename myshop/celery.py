c14a01c6769eb961083f6d1b21806344deb21d0f
Flynotfly <Alteria2004@gmail.com>
2024-08-05 17:37:57 +0400

    Debug - celery.py

--- myshop/celery.py
+++ myshop/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 
 # set default Django settings module for the 'celery' program.
-os.environ.setdefault('django.conf:settings', 'myshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 app = Celery('myshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
