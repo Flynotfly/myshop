7707bd227fb3854d05a3d7b731c40d567a87549d
Flynotfly <Alteria2004@gmail.com>
2024-08-07 09:38:54 +0400

    Create payment process view

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from decimal import Decimal
 
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+
+from orders.models import Order
+
+
+def process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == 'POST':
+        success_url = request.build_absolute_uri(reverse('payment:completed'))
+        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
+        session_data = {
+            'mode': 'payment',
+            'client_reference_id': order.id,
+            'success_url': success_url,
+            'cancel_url': cancel_url,
+            'line_items': []
+        }
+        for item in order.items.all():
+            session_data['line_items'].append(
+                {
+                    'price_data': {
+                        'unit_amount': int(item.price * Decimal('100')),
+                        'currency': 'usd',
+                    },
+                    'product_data': item.product.name,
+                    'quantity': item.quantity,
+                }
+            )
+            # TODO create session process for ourpaymentserver
+            session = ...
+            return redirect(session.url, code=303)
+    else:
+        return render(
+            request,
+            'payment/process.html',
+            locals()
+        )
+
