18e5632467bc16f04af581935f6edf7ffad25a72
Flynotfly <Alteria2004@gmail.com>
2024-08-08 16:39:41 +0400

    Add order detail to admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -34,6 +36,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -47,6 +54,7 @@ class OrderAdmin(admin.ModelAdmin):
         'paid',
         'created',
         'updated',
+        order_detail,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
