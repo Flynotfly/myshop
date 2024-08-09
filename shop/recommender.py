863deb7fdf81a22d7047b9026ce63fb32525dcbc
Flynotfly <Alteria2004@gmail.com>
2024-08-09 19:32:26 +0400

    Create Recommender class and products_bought func

--- shop/recommender.py
+++ shop/recommender.py
@@ -1,6 +1,10 @@
 import redis
 
+from collections.abc import Iterable
+
 from django.conf import settings
+
+from .models import Product
 
 
 r = redis.Redis(
@@ -9,3 +13,14 @@ r = redis.Redis(
     db=settings.REDIS_DB
 )
 
+
+class Recommender:
+    def get_product_key(self, id) -> str:
+        return f'product:{id}:purchased_with'
+
+    def products_bought(self, products: Iterable[Product]) -> None:
+        products_ids = [p.id for p in products]
+        for product_id in products_ids:
+            for with_id in products_ids:
+                if product_id != with_id:
+                    r.zincrby(self.get_product_key(product_id), 1, with_id)
