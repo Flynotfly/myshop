434021a18870ce539c06b2adc2c320c41c452961
Flynotfly <Alteria2004@gmail.com>
2024-08-11 14:27:36 +0400

    Add recommended products to cart_detail view

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recommender import Recommender
 
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -48,11 +49,19 @@ def cart_detail(request):
         )
     coupon_apply_form = CouponApplyForm()
 
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if cart_products:
+        recommended_products = r.suggest_products_for(cart_products, max_result=4)
+    else:
+        recommended_products = []
+
     return render(
         request,
         'cart/detail.html',
         {
             'cart': cart,
             'coupon_apply_form': coupon_apply_form,
+            'recommended_products': recommended_products,
         }
     )
