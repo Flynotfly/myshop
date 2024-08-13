d73259444277aa265c985948007cc0403d927485
Flynotfly <Alteria2004@gmail.com>
2024-08-13 15:54:38 +0400

    Add internalization for ProductAdmin of shop

--- shop/admin.py
+++ shop/admin.py
@@ -14,8 +14,11 @@ class CategoryAdmin(TranslatableAdmin):
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'category']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
+
